630d5c94f38c35684b89c9e7b6f58dc8f4e09c29
Francisco Torres <franciscot.p.93@gmail.com>
2021-08-19 01:10:11 -0300

    aprendiendo variables js

---
+++ js/holaMundo.js
@@ -0,0 +1,28 @@
+// esto es un comentario simple o de una sola linea
+
+/* esto es un 
+comentario de 
+multiples lineas */
+
+// mostrar un mensaje en consola
+console.log("hola comision 16i");
+
+// mostrar mensaje en html
+document.write("este es mi primer msj");
+
+// mostrar un msj en una ventana emergente
+// alert("Hola mundo");
+
+// crear una variable (var - let - const)
+
+let comision = "16i";
+
+document.write("<br>comision: "+comision)
+
+// modificar el contenido de una variable
+comision = "i";
+
+document.write("<br>comision: "+comision)
+
+const anio = 2021;
+document.write("<br>Año: "+anio)
