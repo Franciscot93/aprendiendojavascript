89cf26af3e139b9329dc3007e16f5853479f9e0a
Francisco Torres <franciscot.p.93@gmail.com>
2021-08-23 11:56:18 -0300

    primeros ejercicios resueltos

---
+++ js/tareajava1.js
@@ -0,0 +1,12 @@
+alert("UN MENSAJE");
+document.write("Hello World");
+document.write("<br>")
+let numero1=3;
+let numero2=5;
+document.write(numero1+numero2);
+document.write("<br>")
+
+// parseInt
+let numero3 = parseInt(prompt("ingrese un primer numero"));
+let numero4 = parseInt(prompt("ingrese un segundo numero")); 
+document.write(numero3+numero4)
